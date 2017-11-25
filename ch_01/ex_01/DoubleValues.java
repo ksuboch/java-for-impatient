150245139f2c4aa6d1029510338ee786ebcefd31
ksuboch <3dvapper.inbox@gmail.com>
2017-11-26 01:20:43 +0400

    Double arithmetics

---
+++ ch_01/ex_01/DoubleValues.java
@@ -0,0 +1,12 @@
+package ch_01.ex_01;
+
+/**
+ * DoubleValues
+ */
+public class DoubleValues {
+    public static void main(String[] args) {
+        System.out.println( 1.0 / 0.0);
+        System.out.println(-1.0 / 0.0);
+        System.out.println( 0.0 / 0.0);
+    }
+}
