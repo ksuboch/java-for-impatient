93faef912afe0e9708fde8ffe9c9d81909fc4c44
ksuboch <3dvapper.inbox@gmail.com>
2017-11-26 01:35:45 +0400

    Math floorMod(x, y) function

---
+++ ch_01/ex_01/FloorMode.java
@@ -0,0 +1,17 @@
+package ch_01.ex_01;
+
+/**
+ * FloorMode
+ */
+public class FloorMode {
+    public static void main(String[] args) {
+        System.out.println("Math.floorMod( 5, 2) = " + Math.floorMod( 5, 2));
+        System.out.println("Math.floorMod( 4, 2) = " + Math.floorMod( 4, 2));
+        System.out.println("Math.floorMod(-4, 2) = " + Math.floorMod(-4, 2));
+        System.out.println("Math.floorMod(-5, 2) = " + Math.floorMod(-5, 2));
+        System.out.println("Math.floorMod( 5,-2) = " + Math.floorMod( 5,-2));
+        System.out.println("Math.floorMod( 4,-2) = " + Math.floorMod( 4,-2));
+        System.out.println("Math.floorMod(-4,-2) = " + Math.floorMod(-4,-2));
+        System.out.println("Math.floorMod(-5,-2) = " + Math.floorMod(-5,-2));
+    }
+}
